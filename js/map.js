1ddaab78c2a4185f571f92a231d6a1d78992d947
Elias Couppe <elias.couppe@gmail.com>
2018-01-14 20:00:06 +0100

    Implementation fonction d'ajout de couches à la carte

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var map = Gp.Map.load(
+map = Gp.Map.load(
     "viewerDiv",   // identifiant du conteneur HTML
     // options d'affichage de la carte (Gp.MapOptions)
     {
@@ -14,8 +14,57 @@ var map = Gp.Map.load(
          zoom : 4,
          // Couches à afficher
          layersOptions : {
-             "GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD" : {
-             }
+           // Couche photographies aériennes
+           "ORTHOIMAGERY.ORTHOPHOTOS" : {},
+           // Couche photographies aériennes
+           "GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD" : {
+               opacity : 0.5
+           }
+         },
+         controlsOptions : {
+           "layerswitcher" : {
+             div : "viewerDiv",
+             maximised : true
+           },
+           "graticule" : {
+           },
+           "mouseposition" : {
+             div : "viewerDiv",
+             displayAltitude : false
+           },
+           "getfeatureinfo" : {
+             auto : true
+           }
          }
     }
 ) ;
+
+function addKMLLayer(kmlId) {
+  var layers = map.getLayersOptions();
+  // removes all kml layers previously added
+  for (var layerId in layers) {
+    if (layers[layerId].format === "kml" && layerId !== kmlId) {
+      map.removeLayers(layerId);
+    }
+  }
+  // adds kml Layer associated to the clicked button
+  var kmlFilePath = "./map-data/" + kmlId + ".kml";
+  map.addLayers({
+    kmlId : {
+      format : "kml",
+      url : kmlFilePath,
+      title : kmlId,
+      showPointNames : true
+    }
+  });
+}
+
+// selects all buttons to add kml
+var kmlLayersButtons = document.querySelectorAll("button.kmlLayer");
+
+// adds to the button listener to add layer to the map
+for (var i = 0; i < kmlLayersButtons.length; i++) {
+  kmlLayersButtons[i].onclick = function() {
+    addKMLLayer(this.id)
+  }
+}
