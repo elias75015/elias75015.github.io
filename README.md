f952ff503de30d130b9c73a32961af8a26bb999f
elias couppe <elias.couppe@gmail.com>
2022-08-18 20:04:34 +0200

    reorg arborescence + dossier common + maj sdk

--- README.md
+++
@@ -1,8 +0,0 @@
-## Site/Blog de voyage ##
-
-### TODO ###
-
-- Npmiser le projet ?
-- Dépendances en local ?
-- Responsive pour la page infos.html
-- Interface d'ajout / de retrait de données 
---
+++ common/js/map.js
@@ -0,0 +1,235 @@
+/**
+ * Function to create the map
+ */
+function createMap () {
+    return Gp.Map.load(
+    "mainContent",   // identifiant du conteneur HTML
+    // options d'affichage de la carte (Gp.MapOptions)
+    {
+        // clef d'accès à la plateforme
+        apiKey : "essentiels",
+        // centrage de la carte
+        center : {
+            x : -60,
+            y : -18,
+            projection : "CRS:84"
+        },
+        // niveau de zoom de la carte (de 1 à 21)
+        zoom : 4,
+        // Couches à afficher
+        layersOptions : {
+            // Couche national geo
+            "National Geo" : {
+                position : 0,
+                opacity : 1,
+                visibility : true,
+                format :"osm",
+                title : "National Geographic",
+                url : "https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}",
+                crossOrigin : null
+                }
+        },
+        controlsOptions : {
+            "layerswitcher" : {
+                div : "mainContent",
+                maximised : true
+            },
+            "graticule" : {
+            },
+            "getfeatureInfo" : {
+                options : {
+                    auto : true,
+                    hidden : true
+                }
+            }
+        }
+    }
+) ;
+};
+
+
+/**************************** MAP FUNCTIONS ********************************************/
+
+/**
+  * Fonction qui remet tout à zero:
+  * - suppression de toutes les couches présentes
+  * - ajout de la couche de fond National Geo
+  * - ajout de la couche KML de l'itinéraire choisi
+  * - centrage sur cet itinéraire
+  */
+function goTo (clickedElement) {
+    var itiId = clickedElement.id;
+    var layers = map.getLayersOptions();
+
+    // Suppression de toutes les couches précédement ajoutées
+    for (var layerId in layers) {
+        if (layerId !== "National Geo") {
+            map.removeLayers(layerId);
+        }
+    }
+    // réajout de la couche National Geo si elle a été supprimée
+    if (!layers["National Geo"]) {
+        map.addLayers(baseLayers["NAT_GEO"]);
+    }
+
+    // ajout du kml itineraire correspondant
+    var itiKmlFilePath = "./map-data/" + itiId + ".kml";
+    var itiKmlLayerToAdd = {};
+    itiKmlLayerToAdd[itiId] = {
+        format : "kml",
+        url : itiKmlFilePath,
+        title : itiId,
+        zoomToExtent : true,
+        showPointNames : false
+    };
+
+    map.addLayers(itiKmlLayerToAdd);
+}
+
+/**
+  * Fonction qui :
+  * - ajoute un kml s'il n'est pas déja présent
+  * - supprime un kml s'il est déjà présent
+  *
+  */
+function addKMLLayer (clickedElement) {
+    var kmlId = clickedElement.id;
+    var layers = map.getLayersOptions();
+
+    var isLayerAlreadyAdded = false;
+
+    // checks if the clickedlayer is on the map
+    // - yes ? we set isLayerAlreadyAdded to true
+    for (var layerId in layers) {
+        // FIXME workaround to fix position problem, displaying baseLayer over kmlLayers
+        if (layers[layerId].format !== "kml") {
+            layers[layerId].position = 0;
+        }
+        if (layerId === kmlId) {
+            isLayerAlreadyAdded = true;
+        }
+    }
+
+    // if the kmlLayer is not on the map, we add it.
+    // if the kmlLayer is on the map, we remove it.
+    if (isLayerAlreadyAdded === false) {
+        // adds kml Layer associated to the clicked button
+        var kmlFilePath = "./map-data/" + kmlId + ".kml";
+        var kmlLayerToAdd = {};
+        kmlLayerToAdd[kmlId] = {
+            format : "kml",
+            url : kmlFilePath,
+            title : kmlId,
+            zoomToExtent : true,
+            showPointNames : false
+        };
+        map.addLayers(kmlLayerToAdd);
+    } else {
+        map.removeLayers(kmlId);
+    }
+
+}
+
+
+function addBaseLayer (clickedElement) {
+    var baseLayerId = clickedElement.id;
+
+    var layers = map.getLayersOptions();
+
+    // Deleting previous baseLayer
+    for (var layerId in layers) {
+        for (var blId in baseLayers) {
+            // if we add the already current baselayer, we do nothing
+            if (layerId === Object.keys(baseLayers[baseLayerId])[0]) {
+                return;
+            }
+            if (baseLayers[blId][layerId]) {
+                map.removeLayers(layerId);
+                break;
+            }
+        }
+    }
+    // FIXME workaround to fix position problem, displaying baseLayer over kmlLayers
+    baseLayers[baseLayerId][Object.keys(baseLayers[baseLayerId])[0]].position = 0;
+    // Add the baseLayer to the map
+    map.addLayers(baseLayers[baseLayerId]);
+
+}
+
+// Object referencing all the base layers we can add to the map
+// the first level of keys of this object must correspond to the baselayer <li> tag id
+const baseLayers = {
+    "OSM" : {
+        "coucheOSM" : {
+            position : 0,
+            opacity : 1,
+            visibility : true,
+            format :"osm",
+            title : "OpenStreetMap",
+            url : "https://{a-c}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=39cfa021a92541bdbcac02cf76e3a2ce",
+            crossOrigin : null
+            }
+        },
+    "US_ORTHO" : {
+        "USA Orthos" : {
+            position : 0,
+            format : "wmts",
+            title : "Photos US",
+            url : "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/WMTS/tile/1.0.0/USGSImageryOnly/{Style}/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}",
+            layer : "USGSImageryOnly",
+            tileMatrixSet : "default028mm",
+            topLeftCorner : {
+                x : -20037508,
+                y : 20037508
+            },
+            styleName : "default",
+            outputFormat : "image/png",
+            opacity : 1,
+            visibility : true
+        }
+    },
+    "NAT_GEO" : {
+        "National Geo" : {
+            position : 0,
+            opacity : 1,
+            visibility : true,
+            format :"osm",
+            title : "National Geographic",
+            url : "https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}",
+            crossOrigin : null
+            }
+    }
+}
+
+/************** ADD LISTENERS TO DOM  **********************************/
+
+// selects all buttons to add itinerary kml
+var itiLayersButtons = document.querySelectorAll("li.itiLayer");
+
+// adds to the button listener to add layer to the map
+for (var i = 0; i < itiLayersButtons.length; i++) {
+    itiLayersButtons[i].onclick = function () {
+        goTo(this);
+    };
+}
+
+
+// selects all buttons to add kml
+var kmlLayersButtons = document.querySelectorAll("li.kmlLayer");
+
+// adds to the button listener to add layer to the map
+for (var i = 0; i < kmlLayersButtons.length; i++) {
+    kmlLayersButtons[i].onclick = function () {
+        addKMLLayer(this);
+    };
+}
+
+// selects all buttons to add base layer
+var baseLayersButtons = document.querySelectorAll("li.baseLayer");
+
+// adds to the button listener to add layer to the map
+for (var i = 0; i < baseLayersButtons.length; i++) {
+    baseLayersButtons[i].onclick = function () {
+        addBaseLayer(this);
+    };
+}
